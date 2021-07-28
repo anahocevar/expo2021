527ec4c3cbdea7c0fd04f167ee74d3d0b20c58bb
anahocevar <ahocevara@gmail.com>
2021-07-28 16:40:02 +0200

    added celebratory gif and consequent changes

--- app.py
+++ app.py
@@ -1,21 +1,31 @@
 import streamlit as st
+import time
 
-def response(code, condition, mylist=[]):       
+def response(q, code, condition, congrats_gif, scoring, num_qs, mylist=[]):       
     if code=='Pass':
-            return True, False
+        st.session_state[q] = True, False
+        return
 
-    state =  False, False
+    st.session_state[q] =  False, False
     try:
         exec(code)
     except Exception as e:
         st.markdown(f""" Your code has an erorr ({e}). Modify your code and submit again. Hint: if you can't get the right answer but nevertheless want to advance to the next question, you can enter `Pass` as your code and submit that.""")
     else:
         if eval(condition):
-            state = True, True
+            st.session_state[q] = True, True
+            score = sum([getattr(st.session_state, 'q'+str(i))[1] for i in range(1, num_qs+1)])
+            scoring.markdown(f"""Congrats! Your score is **{score} point{'s' if score > 1 else ''}**.\n
+Can you also get the next question?""")
+            play_gif(congrats_gif)
         else:
             st.markdown(""" Wrong answer, modify your code and submit again. Hint: if you can't get the right answer but nevertheless want to advance to the next question, you can enter `Pass` as your code and submit that. """)
-    return state
 
+def play_gif(congrats_gif):
+    congrats_gif.image(
+                "https://media.giphy.com/media/3o6fIUZTTDl0IDjbZS/giphy.gif")
+    time.sleep(2)
+    congrats_gif.empty()
 
 def app():
     
@@ -31,8 +41,13 @@ def app():
 
     st.sidebar.markdown("""# Your Scoreboard""")
     scoring = st.sidebar.empty()
+    congrats_gif = st.sidebar.empty()
     scoring.markdown(""" You haven't earned any points yet. \n 
     Try to answer at least the first question!""")
+    if sum([getattr(st.session_state, 'q'+str(i))[1] for i in range(1, num_qs+1)]):
+        score = sum([getattr(st.session_state, 'q'+str(i))[1] for i in range(1, num_qs+1)])
+        scoring.markdown(f"""Congrats! Your score is **{score} point**.\n
+Can you also get the next question?""")
 
     st.markdown(""" ## Question 1 """)
     st.markdown(""" A list is one of the basic data structures in Python. It can be created using square brackets and data items separated with commas. Like this:""")
@@ -44,14 +59,9 @@ def app():
     mylist = [1,2,3]
     q1_code = st.text_area('', key='q1_text')
     if st.button('Submit', key='1'):
-        st.session_state.q1 = response(q1_code, 'mylist==[1,2,3,33]', mylist)
+        response('q1', q1_code, 'mylist==[1,2,3,33]', congrats_gif, scoring, num_qs, mylist)
 
-    if st.session_state.q1[0]:
-        if st.session_state.q1[1]:
-            st.session_state.score = sum([getattr(st.session_state, 'q'+str(i))[1] for i in range(1, num_qs+1)])
-            scoring.markdown(f"""Congrats! Your score is **{st.session_state.score} point**.\n
-Can you also get the next question?""")
-
+    if st.session_state.q1[0]:            
         st.markdown(""" ## Question 2 """)
         st.markdown(""" When you want to write multiple commands, you need to write them in separate lines. Let's say we wanted to add 4 as well as 5 to our list, we'd write:""")
         st.code("""mylist.append(4)
@@ -60,14 +70,9 @@ mylist.append(5)""", language='python')
 
         q2_code = st.text_area('', key='q2_text')
         if st.button('Submit', key='2'):
-            st.session_state.q2 = response(q2_code, 'mylist==[1,2,3,66,99]', mylist)
-    
+            response('q2', q2_code, 'mylist==[1,2,3,66,99]', congrats_gif, scoring, num_qs, mylist)    
                 
-    if st.session_state.q2[0]:
-        if st.session_state.q2[1]:
-            st.session_state.score = sum([getattr(st.session_state, 'q'+str(i))[1] for i in range(1, num_qs+1)])
-            scoring.markdown(f"""Well done! Your score is **{st.session_state.score} points**.\n
-Can you also get the next question?""")
+    if st.session_state.q2[0]:            
         st.markdown(""" ## Question 3 """)
         st.markdown(""" Under construction...""")
         
